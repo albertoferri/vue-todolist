0ffa1cdd71bcdfaf38065a3a04916ccea1829941
albertoferri <falberto99@gmail.com>
2024-02-20 14:02:25 +0100

    finita milestone 1

--- js/script.js
+++ js/script.js
@@ -3,7 +3,28 @@ const {createApp} = Vue;
 createApp({
     data() {
       return {
-        
+        todos: [
+            {
+                text: 'Impara le basi di un nuovo linguaggio di programmazione (Python, Java, C++, etc.).',
+                done: false 
+            },
+            {
+                text: 'Segui un tutorial online per creare un semplice programma.',
+                done: true 
+            },
+            {
+                text: 'Iscriviti a un corso di informatica per approfondire le tue conoscenze.',
+                done: false 
+            },
+            {
+                text: 'Leggi un libro su un argomento di tecnologia che ti interessa.',
+                done: false 
+            },
+            {
+                text: "Guarda un documentario sulla storia dell'informatica.",
+                done: true 
+            }
+        ]
       };
     },
 }).mount("#app");
