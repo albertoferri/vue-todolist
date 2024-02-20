5d32e77014ac5225ef661a44dd33e8f3164dc520
albertoferri <falberto99@gmail.com>
2024-02-20 16:02:18 +0100

    MILESTONE 3 AGGIUNTA

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
+// MILESTONE 1
+// Stampare all'interno di una lista HTML un item per ogni todo.
+// Se la proprietà done è uguale a true, visualizzare il testo del todo sbarrato.
 const {createApp} = Vue;
 
 createApp({
@@ -24,12 +27,26 @@ createApp({
                 text: "Guarda un documentario sulla storia dell'informatica.",
                 done: true 
             }
-        ]
+        ],
+        newTodoText: ''
       };
     },
     methods: {
+        // MILESTONE 2
+        // Visualizzare a fianco ad ogni item ha una ":x:": cliccando su di essa, il todo viene rimosso dalla lista.
         deleteTodo(index) {
           this.todos.splice(index, 1);
-        }
-    }
-}).mount("#app");
+        },
+
+        // MILESTONE 3
+        // Predisporre un campo di input testuale e un pulsante "aggiungi": cliccando sul pulsante, 
+        // il testo digitato viene letto e utilizzato per creare un nuovo todo, che quindi viene aggiunto alla lista dei todo esistenti.
+        addTodo() {
+            const trimmedText = this.newTodoText.trim();
+            if (trimmedText !== '') {
+              this.todos = [...this.todos, { text: trimmedText, done: false }];
+              this.newTodoText = '';
+            }
+        },
+      }
+    }).mount('#app');
